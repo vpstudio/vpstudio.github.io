26b39bd5321870db7c8223a392f7f894d73dff2f
vpstudio <kevinkorner@Kevins-MacBook-Pro.local>
2016-04-22 11:03:05 -0700

    Roar cool looking things

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,50 @@
 $(document).ready(function(){
+var count = 0;
 
 $("#menu-item").click(function(){
 	if ($('#side-bar').hasClass('side-bar-active') ){
 		$("#side-bar").removeClass("side-bar-active");
 		$(".content-overlay").removeClass("content-overlay-active");
+    caroulwidth = parseInt($('#carousel').css('width').replace("px", ""))/5;
+    locs = [0, caroulwidth * -1, caroulwidth * -2, caroulwidth * -3, caroulwidth * -4];
+    $("#carousel").css('margin-left',locs[count]);
 	} else {
 		$("#side-bar").addClass("side-bar-active");
 		$(".content-overlay").addClass("content-overlay-active");
+    caroulwidth = parseInt($('#carousel').css('width').replace("px", ""))/5;
+    locs = [0, caroulwidth * -1, caroulwidth * -2, caroulwidth * -3, caroulwidth * -4];
+    $("#carousel").css('margin-left',locs[count]);
 
     }
 
 });
 
 $("#carousel-next").click(function() {
-  mval = parseInt($('#carousel').css('margin-left').replace("px", ""));
   caroulwidth = parseInt($('#carousel').css('width').replace("px", ""))/5;
-  if (mval == -(4*caroulwidth)) {
+  locs = [0, caroulwidth * -1, caroulwidth * -2, caroulwidth * -3, caroulwidth * -4];
+  if (count == 4) {
+    $("#carousel").css('margin-left',locs[count]);
     return false
   } else {
-    $("#carousel").css('margin-left',mval - caroulwidth);
+    count = count + 1;
+    $("#carousel").css('margin-left',locs[count]);
   }
 });
 
   //Implement the "slide to right" when the user clicks on #carousel-prev here
 $("#carousel-prev").click(function() {
-  mval = parseInt($('#carousel').css('margin-left').replace("px", ""));
   caroulwidth = parseInt($('#carousel').css('width').replace("px", ""))/5;
-  if (mval == 0) {
+  locs = [0, caroulwidth * -1, caroulwidth * -2, caroulwidth * -3, caroulwidth * -4];
+
+  if (count == 0) {
+    $("#carousel").css('margin-left',0);
     return false
   } else {
-    $("#carousel").css('margin-left',mval + caroulwidth);
+    count = count - 1;
+    $("#carousel").css('margin-left',locs[count]);
   }
 });
+
 
 
 });
